1085b9931ce0a1bf6a7cc61e1c6c60f5d78fc910
meshonline <artgolf1000@gmail.com>
2016-12-09 11:32:52 +0800

    Add blur path and calculate GI by distance

--- SSGIForward.glsl
+++ SSGIForward.glsl
@@ -21,10 +21,11 @@ void VS()
 #ifdef COMPILEPS
 
 vec3 normal_from_depth(vec2 texcoords) {
-    // One pixel: 0.001 = 1 / 1000 (pixels)
+    // Delta coordinate of one pixel: 0.001 = 1 (pixel) / 1000 (pixels)
     const vec2 offset1 = vec2(0.0, 0.001);
     const vec2 offset2 = vec2(0.001, 0.0);
     
+    // Fetch depth from depth buffer
     float depth = DecodeDepth(texture2D(sDepthBuffer, texcoords).rgb);
     float depth1 = DecodeDepth(texture2D(sDepthBuffer, texcoords + offset1).rgb);
     float depth2 = DecodeDepth(texture2D(sDepthBuffer, texcoords + offset2).rgb);
@@ -32,19 +33,25 @@ vec3 normal_from_depth(vec2 texcoords) {
     vec3 p1 = vec3(offset1, depth1 - depth);
     vec3 p2 = vec3(offset2, depth2 - depth);
     
+    // Calculate normal
     highp vec3 normal = cross(p1, p2);
     normal.z = -normal.z;
     
     return normalize(normal);
 }
 
-vec3 normal_from_pixels(vec2 texcoords1, vec2 texcoords2) {
+vec3 normal_from_pixels(vec2 texcoords1, vec2 texcoords2, out float dist) {
+    // Fetch depth from depth buffer
     float depth1 = DecodeDepth(texture2D(sDepthBuffer, texcoords1).rgb);
     float depth2 = DecodeDepth(texture2D(sDepthBuffer, texcoords2).rgb);
     
+    // Calculate normal
     highp vec3 normal = vec3(texcoords2 - texcoords1, depth2 - depth1);
     normal.z = -normal.z;
-    
+
+    // Calculate distance between texcoords
+    dist = length(normal);
+
     return normalize(normal);
 }
 
@@ -53,16 +60,21 @@ vec3 Calculate_GI(vec3 pixel_normal, vec2 coord)
     vec3 light_color;
     vec3 pixel_to_light_normal;
     vec3 light_normal, light_to_pixel_normal;
+    float dist;
+
     // Get the pixel color
     light_color = texture2D(sDiffMap, coord).rgb;
     // Calculate normal for the pixel
     light_normal = normal_from_depth(coord);
     // Calculate normal from the pixel to current pixel
-    light_to_pixel_normal = normal_from_pixels(coord, vScreenPos);
+    light_to_pixel_normal = normal_from_pixels(coord, vScreenPos, dist);
     // Calculate normal from current pixel to the pixel
     pixel_to_light_normal = -light_to_pixel_normal;
+
     // Calculate GI
-    return light_color * max(0.0, dot(light_normal, light_to_pixel_normal)) * max(0.0, dot(pixel_normal, pixel_to_light_normal));
+    vec3 gi = light_color * max(0.0, dot(light_normal, light_to_pixel_normal)) * max(0.0, dot(pixel_normal, pixel_to_light_normal)) / dist;
+    
+    return gi;
 }
 
 void PS()
@@ -75,14 +87,15 @@ void PS()
     // Prepare to accumulate GI
     gi = vec3(0.0);
  
-    // Accumulate GI
-    for (int y=0; y<8; ++y) {
-        for (int x=0; x<8; ++x) {
-            gi += Calculate_GI(pixel_normal, vec2((float(x)+0.5)/8.0, (float(y)+0.5)/8.0));
+    // Accumulate GI from some uniform samples
+    for (int y=0; y<16; ++y) {
+        for (int x=0; x<16; ++x) {
+            gi += Calculate_GI(pixel_normal, vec2((float(x)+0.5)/16.0, (float(y)+0.5)/16.0));
         }
     }
     
-    gi *= 0.1;
+    // Make GI not too strong
+    gi /= 64.0;
     
     gl_FragColor = vec4(gi, 1.0);
 }
